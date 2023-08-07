2733392db7281856837de81dd366800f22096c7d
sussa3007 <sussa1933@gmail.com>
2023-08-07 16:47:57 +0900

    feat: Order Service 로직 구현

---
+++ src/main/java/com/paypilot/paypilot/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.paypilot.paypilot.service;
+
+import com.paypilot.paypilot.constants.OrderStatus;
+import com.paypilot.paypilot.constants.PaymentLandingPage;
+import com.paypilot.paypilot.dto.CreateOrderResponseDto;
+import com.paypilot.paypilot.dto.OrderDetailsResponseDto;
+import com.paypilot.paypilot.dto.OrderDto;
+import com.paypilot.paypilot.dto.PaypalAppContextDto;
+import com.paypilot.paypilot.entity.Order;
+import com.paypilot.paypilot.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    private final PaypalHttpClient paypalHttpClient;
+
+    public CreateOrderResponseDto createOrder(OrderDto orderDto) throws Exception {
+        PaypalAppContextDto defaultContext = PaypalAppContextDto.getDefaultContext();
+        orderDto.setApplicationContext(defaultContext);
+        CreateOrderResponseDto orderResponse = paypalHttpClient.createOrder(orderDto);
+        Order order = Order.createOrder(orderResponse);
+        orderRepository.save(order);
+        return orderResponse;
+    }
+
+    public void approvedOrder(String orderId) {
+        Order targetOrder = orderRepository.findByPaypalOrderId(orderId);
+        targetOrder.setPaypalOrderStatus(OrderStatus.APPROVED.toString());
+        orderRepository.save(targetOrder);
+    }
+
+    public OrderDetailsResponseDto getOrderDetails(String orderId) throws Exception {
+        return paypalHttpClient.getOrderDetails(orderId);
+    }
+}
