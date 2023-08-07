8f7d961f83085d26fcdbc8f1c64d3584b4a490f4
sussa3007 <sussa1933@gmail.com>
2023-08-07 16:23:00 +0900

    feat: Paypal AccessToeknResponseDto 작성

---
+++ src/main/java/com/paypilot/paypilot/dto/AccessTokenResponseDto.java
@@ -0,0 +1,33 @@
+package com.paypilot.paypilot.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccessTokenResponseDto {
+
+    private String scope;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("app_id")
+    private String applicationId;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+
+    private String nonce;
+
+    @JsonIgnore
+    private Instant expiration;
+
+}
