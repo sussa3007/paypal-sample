8092583b418ce7aad0721732b50e48e708bb89af
sussa3007 <sussa1933@gmail.com>
2023-08-16 16:47:38 +0900

    feat: PurchaseUnit 필드 추가

--- src/main/java/com/paypilot/paypilot/dto/PurchaseUnit.java
+++ src/main/java/com/paypilot/paypilot/dto/PurchaseUnit.java
@@ -1,9 +1,14 @@
 package com.paypilot.paypilot.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class PurchaseUnit {
+
+    private List<OrderItemDto> items;
 
     private MoneyDto amount;
 
